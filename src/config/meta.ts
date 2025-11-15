2838b73ff8c8efe288f300d271f1d631c1588ee2
Roko Ponjarac <72657685+Roko03@users.noreply.github.com>
2025-11-16 00:57:27 +0100

    Initial commit

---
+++ src/config/meta.ts
@@ -0,0 +1,17 @@
+interface Meta {
+  name: string;
+  title: string;
+  titleTemplate: string;
+  description: string;
+  url: string;
+}
+
+const meta: Meta = {
+  name: 'Roko',
+  title: 'Roko | Next.js Boilerplate',
+  titleTemplate: '%s | Roko03',
+  description: 'This project was bootstrapped with create-next-app.',
+  url: 'https://github.com/Roko03',
+};
+
+export default meta;
